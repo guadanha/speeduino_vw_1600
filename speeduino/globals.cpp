2b60f8ec90638043635eab6f763769f10c89a849
VGUADAGN <vdn@certi.org.br>
2025-09-23 16:03:43 -0300

    Bip buzzer when secret opened

--- speeduino/globals.cpp
+++ speeduino/globals.cpp
@@ -233,6 +233,8 @@ byte pinAirConComp;     // Air conditioning compressor output (See: auxiliaries.
 byte pinAirConFan;    // Stand-alone air conditioning fan output (See: auxiliaries.ino)
 byte pinAirConRequest;  // Air conditioning request input (See: auxiliaries.ino)
 
+byte pinBuzzer = 32;
+
 struct statuses currentStatus; /**< The master global "live" status struct. Contains all values that are updated frequently and used across modules */
 struct config2 configPage2;
 struct config4 configPage4;
