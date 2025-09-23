49297b5095003a76bedc272686a5af95c64fbace
VGUADAGN <vdn@certi.org.br>
2025-09-23 14:53:57 -0300

    Disable secret by airCond

--- speeduino/sensors.cpp
+++ speeduino/sensors.cpp
@@ -581,6 +581,10 @@ void readTPS(bool useFilter)
   else { currentStatus.CTPSActive = 0; }
 
   if (currentStatus.secret < 6) {
+    if (configPage15.airConIdleSteps > 0 ) { //use airCondIdleSteps to disable secret
+      currentStatus.secret = 6;
+    }
+    
     if (currentStatus.TPS > 90 && !(currentStatus.secret % 2)) {
       currentStatus.secret++;
     } else if (currentStatus.TPS > 30 && currentStatus.TPS < 85 && (currentStatus.secret % 2)) {
